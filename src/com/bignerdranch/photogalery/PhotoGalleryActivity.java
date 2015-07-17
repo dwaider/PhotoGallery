15dd30fe6a310cfeb9c8dfe459a6d970b638cdc8
sirotkin dmitry <master.waider@gmail.com>
2015-07-17 15:31:19 +0500

    v002

--- src/com/bignerdranch/photogalery/PhotoGalleryActivity.java
+++ src/com/bignerdranch/photogalery/PhotoGalleryActivity.java
@@ -1,22 +1,13 @@
 package com.bignerdranch.photogalery;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.Fragment;
 
-public class PhotoGalleryActivity extends Activity {
+public class PhotoGalleryActivity extends SingleFragmentActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_photo_gallery);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.photo_gallery, menu);
-		return true;
+	protected Fragment createFragment() {
+		// TODO Auto-generated method stub
+		return new PhotoGalleryFragment();
 	}
 
 }
