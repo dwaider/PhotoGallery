004180c11e6db5cbf0f419f8ab0cd3bfe367a660
sirotkin dmitry <master.waider@gmail.com>
2015-07-21 13:22:32 +0500

    v004

--- src/com/bignerdranch/photogalery/PhotoGalleryFragment.java
+++ src/com/bignerdranch/photogalery/PhotoGalleryFragment.java
@@ -1,7 +1,11 @@
 package com.bignerdranch.photogalery;
 
+import java.io.IOException;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +13,13 @@ import android.widget.GridView;
 
 public class PhotoGalleryFragment extends Fragment {
 	GridView mGridView;
+	private static final String TAG = "PhotoGalleryFragment";
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
      	setRetainInstance(true);
+     	new FetchItemsTask().execute();
 	}
 	
 	@Override
@@ -21,5 +27,18 @@ public class PhotoGalleryFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_photo_gallery, container,false);
 		mGridView = (GridView)v.findViewById(R.id.gridView);
 		return v;
+	}
+	
+	private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+			  String result = new FlickrFetchr().getUrl("http://www.google.com");
+			  Log.i(TAG, "Fetched contents of URL: " + result);
+			} catch (IOException ioe) {
+			  Log.e(TAG, "Failed to fetch URL: ", ioe);
+			}
+		  return null;
+		}
 	}
 }
