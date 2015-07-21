c7b4da89b866a0fe94c1cdc85ca76589f192be23
dwaider <master.waider@gmail.com>
2015-07-21 22:53:09 +0600

    v005

--- src/com/bignerdranch/photogalery/FlickrFetchr.java
+++ src/com/bignerdranch/photogalery/FlickrFetchr.java
@@ -6,11 +6,21 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import android.net.Uri;
+import android.util.Log;
+
 //сетевые взаимодействия PhotoGallery будут обеспечивать-
 //ся одним классом FlickrFetchr
 //FlickrFetchr будет состоять всего из двух методов:
 //	getUrlBytes(String) и getUrl(String).
 public class FlickrFetchr {
+	public static final String TAG = "FlickrFetchr";
+	private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
+	private static final String API_KEY = "f0f46515b3123cfc8a030566f0789ae3";
+	private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+	private static final String PARAM_EXTRAS = "extras";
+	private static final String EXTRA_SMALL_URL = "url_s";//url_s приказывает Flickr включить URL 
+	//уменьшенной версии изображения, если она доступна
 	//получает	низкоуровневые данные по URL и возвращает их в виде массива байтов.
 	byte[] getUrlBytes(String urlSpec) throws IOException {
 		//создает объект URL на базе строки — например, http://www.google.com
@@ -51,5 +61,19 @@ public class FlickrFetchr {
 	//преобразует результат из getUrlBytes(String) в String
 	public String getUrl(String urlSpec) throws IOException {
 		return new String(getUrlBytes(urlSpec));
+	}
+	
+	public void fetchItems() {
+		try {
+			String url = Uri.parse(ENDPOINT).buildUpon()
+			.appendQueryParameter("method", METHOD_GET_RECENT)
+			.appendQueryParameter("api_key", API_KEY)
+			.appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+			.build().toString();
+  		    String xmlString = getUrl(url);
+		    Log.i(TAG, "Received xml: " + xmlString);
+		} catch (IOException ioe) {
+		    Log.e(TAG, "Failed to fetch items", ioe);
+		}
 	}
 }
